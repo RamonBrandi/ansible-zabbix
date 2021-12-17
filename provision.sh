a8a05f0c2764c2f0f5581ce13c57ef9a900aacfc
ramones <ramonbrand@gmail.com>
2021-12-17 00:13:54 -0300

    my first commit

--- provision.sh
+++ provision.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-KEY_PATH='/vagrant/files'
-mkdir -p /root/.ssh
-cp $KEY_PATH/key /root/.ssh/id_rsa
-cp $KEY_PATH/key.pub /root/.ssh/id_rsa.pub
-cp $KEY_PATH/key.pub /root/.ssh/authorized_keys
-chmod 400 /root/.ssh/id_rsa*
-cat /root/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-
-# Garantindo os hosts
 HOSTS=$(head -n7 /etc/hosts)
 echo -e "$HOSTS" > /etc/hosts
 echo '10.20.20.100 server-1.emp.example' >> /etc/hosts
@@ -18,7 +9,9 @@ echo '10.20.20.130 server-4.emp.example' >> /etc/hosts
 
 #ansible
 apt-get update
-apt-get install -y software-properties-common
-sudo apt-add-repository –yes –update ppa:ansible/ansible
-sudo apt install -y ansible
-cp ansible ~/
+apt install python3
+apt install python3-pip
+pip install ansible
+
+#git
+git clone https://github.com/RamonBrandi/ansible-zabbix.git
